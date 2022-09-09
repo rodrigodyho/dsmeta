ec3185382afb25a15ce088a55c3382d829212606
Rodrigo Lima <rodrigodyho@gmail.com>
2022-09-09 13:52:48 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
-import NotificationButton from "./componentes/NotificationButton"
+import SalesCard from "./componentes/SalesCard"
+import Header from "./componentes/Header"
+
 
 function App() {
   return (
     <>
-      <h1>Olá Mundo!</h1>
-      <NotificationButton />
-    </> 
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
 
